7876e335f1ff1f4e27894533ec99ae2c8e444ca6
shyam <yash.shyam007@gmail.com>
2025-07-20 22:58:13 +0530

    Length of longest substring without repeating characters - brute force method

--- Day1.js
+++ Day1.js
@@ -1,1 +1,22 @@
-// Welcome to coding world!
+// length of the longest substring without repeating characters
+var s = "tmmzuxt";
+var c = s.split("")
+var x = []
+var l = x.length
+for (var i = 0; i < c.length; i++) {
+    var y = []
+    y.push(c[i])
+    for (var j = i + 1; j < c.length; j++) {
+        if (c[j].length > 0 && y.indexOf(c[j]) == -1) {
+            y.push(c[j])
+        }
+        else {
+            break;
+        }
+    }
+    if (y.length > l) {
+        l = y.length
+    }
+}
+console.log(l)
+
